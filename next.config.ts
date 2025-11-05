a36c088416174a4d4799be202dad0f17934b741d
Antoine Pollet <pollet.antoine.alexis@gmail.com>
2025-11-05 11:35:32 +0100

    nfr: Image path

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // GitHub Pages requires unoptimized images unless using a custom image loader
+  output: 'export',
   images: {
-    unoptimized: true,
+    unoptimized: true
   },
 };
 
